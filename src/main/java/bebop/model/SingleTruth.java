4f3de134f77ebd048b44d5b9b68b0b6df6509bc5
Teletha <Teletha.T@gmail.com>
2017-02-13 16:30:27 +0900

    How about model?

---
+++ src/main/java/bebop/model/SingleTruth.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2016 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package bebop.model;
+
+import javafx.beans.property.ListProperty;
+
+import kiss.Manageable;
+import kiss.Singleton;
+
+/**
+ * @version 2017/02/13 16:04:52
+ */
+@Manageable(lifestyle = Singleton.class)
+public class SingleTruth {
+
+    public int value = 0;
+
+    public SingleTruth plus(int additionalValue) {
+        return 
+    }
+}
